6939d650bb51605a238d72049cc1881ad01eda85
abhishek13sang-sketch <abhishek13sang@gmail.com>
2026-05-05 12:30:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let login = document.querySelector(".login");
 let register = document.querySelector(".register");
 let get = document.querySelector(".get");
 let learn = document.querySelector(".learn");
+let about = document.querySelector(".about");
+
+about.addEventListener("click",()=>{
+    document.location.href = "about.html";
+})
 
 learn.addEventListener("click",()=>{
     document.location.href = "register.html";
